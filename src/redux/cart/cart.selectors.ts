9a0a310125b20d4ecca3ff386534dfdd272a195e
maximzadoina <maxim.zadoina@gmail.com>
2020-08-20 20:32:12 +0300

    added selectors

---
+++ src/redux/cart/cart.selectors.ts
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartITEM
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
